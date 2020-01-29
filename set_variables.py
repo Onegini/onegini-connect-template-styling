3fbd769faa04e58bcd747e035b0a463674ecface
Erik Veerman <erik.veerman@onegini.com>
2020-01-29 11:41:58 +0100

    Sciptin

--- set_variables.py
+++ set_variables.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from os import environ
+import os
+import subprocess
 import json
 import argparse
 
@@ -14,16 +15,31 @@ def loadJson( configuration ='./config.json'):
         with open(configuration) as json_file:
             return json.load(json_file)
     except FileNotFoundError:
-        if (configuration != './config.json'):
+        if (configuration == './config.json'):
+            print('Cannot find default config.json file. Please make sure it is in the same folder as this script')
+        else:
             print('File %s was not found. Using default instead' % configuration)
             return loadJson()
-        else:
-            print('Cannot find default config.json file. Please make sure it is in the same folder as this script')
 
 def loadVars (data, vars='default'):
-    print(data[vars])
+    try:
+        entries = data[vars]
+        for entry in entries:
+            # print(entry, entries[entry])
+            os.environ[entry] = entries[entry]
+    except KeyError:
+        print('Cannot find the environment you are looking for in current configuration file.')
+
 
 parsed = loadJson(arguments.config) if arguments.config  else loadJson()
 
 loadVars(parsed)
 if arguments.environment: loadVars(parsed, arguments.environment)
+
+# npm1 = subprocess.Popen(["npm i"], shell=True, stdout=subprocess.PIPE)
+# subprocess.Popen(["xterm"], shell=True, stdin=npm1.stdout)
+
+mvn1 = subprocess.Popen(["mvn spring-boot:run"], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+mvn2 = subprocess.Popen(["open -a terminal"], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+mvn1_out = mvn1.communicate()
+mvn2_in = mvn2.communicate(input=mvn1_out[0])
